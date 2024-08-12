26d9dfef0cf0c2d922ac83ad17349181d1013c39
WonSeok-dd <cluejws@gmail.com>
2024-08-12 22:42:32 +0900

    [#1] build: 모듈 생성 및 삭제
    - module-api
        - user-api
        - post-api
    - module-core
        - common

--- module-api/post-api/src/main/java/com/soundie/PostApiApplication.java
+++ module-api/post-api/src/main/java/com/soundie/PostApiApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestApplication {
+public class PostApiApplication {
     public static void main(String[] args) {
-        SpringApplication.run(RestApplication.class, args);
+        SpringApplication.run(PostApiApplication.class, args);
     }
 }
---
+++ module-core/common/src/main/java/com/soundie/Main.java
@@ -0,0 +1,7 @@
+package com.soundie;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
