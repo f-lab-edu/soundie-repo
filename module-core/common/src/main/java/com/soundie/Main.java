a5cc3c4de2b6b7d8d219041719556d3e6bd41a79
WonSeok-dd <cluejws@gmail.com>
2024-08-13 17:19:08 +0900

    [#1] build: 모듈 수정
    - module-core
    - module-api

--- module-core/common/src/main/java/com/soundie/Main.java
+++
@@ -1,7 +0,0 @@
-package com.soundie;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- module-core/src/main/java/com/soundie/Application.java
+++ module-core/src/main/java/com/soundie/Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PostApiApplication {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(PostApiApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
