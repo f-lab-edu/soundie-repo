496b019599653d9d6b92fc64ece7650ece6ce29a
WonSeok-dd <cluejws@gmail.com>
2024-08-21 05:24:08 +0900

    [#6] test: 음원 게시물 관련(컨트롤러)
    - 음원 게시물 목록 조회

---
+++ module-api/src/test/java/com/soundie/post/controller/PostControllerTest.java
@@ -0,0 +1,45 @@
+package com.soundie.post.controller;
+
+import com.soundie.post.dto.GetPostResDto;
+import com.soundie.post.service.PostService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = PostController.class)
+class PostControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PostService postService;
+
+    @DisplayName("음원 게시물 목록을 조회한다.")
+    @Test
+    void readPostList() throws Exception {
+        // given
+        GetPostResDto result = GetPostResDto.of(List.of());
+
+        given(postService.readPostList()).willReturn(result);
+
+        // when // then
+        mockMvc.perform(
+                        get("/api/posts")
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.message").value("success"))
+                .andExpect(jsonPath("$.data").isMap());
+    }
+}
