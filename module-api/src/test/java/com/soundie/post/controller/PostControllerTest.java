974a0e68258a231a6554f34465a30099c2d35d9d
WonSeok-dd <cluejws@gmail.com>
2024-08-21 18:39:28 +0900

    [#6] test: 음원 게시물 관련(컨트롤러)
    - 음원 게시물 등록
    - 음원 게시물 좋아요 달기

--- module-api/src/test/java/com/soundie/post/controller/PostControllerTest.java
+++ module-api/src/test/java/com/soundie/post/controller/PostControllerTest.java
@@ -1,21 +1,27 @@
 package com.soundie.post.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.soundie.member.domain.Member;
 import com.soundie.post.domain.Post;
-import com.soundie.post.dto.GetPostDetailResDto;
-import com.soundie.post.dto.GetPostResDto;
+import com.soundie.post.dto.*;
 import com.soundie.post.service.PostService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -25,6 +31,9 @@ class PostControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @MockBean
     private PostService postService;
@@ -73,4 +82,78 @@ class PostControllerTest {
                 .andExpect(jsonPath("$.code").value("200"))
                 .andExpect(jsonPath("$.message").value("success"));
     }
+
+    @DisplayName("음원 게시물을 등록한다.")
+    @Test
+    void createPost() throws Exception {
+        // given
+        Long memberId = 1L;
+        Long postId = 1L;
+
+        PostPostCreateReqDto request = postPostCreateReqDto();
+        PostIdElement response = postIdElement(postId);
+        given(postService.createPost(eq(memberId), any(PostPostCreateReqDto.class))).willReturn(response);
+
+        // when // then
+        MultiValueMap <String, String> params = new LinkedMultiValueMap<>();
+        params.add("memberId", String.valueOf(memberId));
+        mockMvc.perform(
+                post("/api/posts")
+                        .params(params)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.message").value("success"))
+                .andExpect(jsonPath("$.data.postId").value(1L));
+
+    }
+
+    @DisplayName("음원 게시물에 좋아요를 누른다.")
+    @Test
+    void likePost() throws Exception {
+        // given
+        Long memberId = 1L;
+        Long postId = 1L;
+        Number likeCount = 1;
+        Boolean liked = true;
+
+        PostPostLikeResDto response = postPostLikeResDto(likeCount, liked);
+        given(postService.likePost(eq(memberId), eq(postId))).willReturn(response);
+
+        // when // then
+        MultiValueMap <String, String> params = new LinkedMultiValueMap<>();
+        params.add("memberId", String.valueOf(memberId));
+        mockMvc.perform(
+                        post("/api/posts/" + postId + "/like")
+                                .params(params)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.message").value("success"))
+                .andExpect(jsonPath("$.data.likeCount").value(1))
+                .andExpect(jsonPath("$.data.liked").value(true));
+    }
+
+    private PostPostCreateReqDto postPostCreateReqDto(){
+        return PostPostCreateReqDto.builder()
+                .title("노래 제목")
+                .musicPath("노래 주소")
+                .albumImgPath("앨범 이미지 주소")
+                .albumName("앨범 이름")
+                .build();
+    }
+
+    private PostIdElement postIdElement(Long postId){
+        return PostIdElement.of(postId);
+    }
+
+    private PostPostLikeResDto postPostLikeResDto(Number likeCount, Boolean liked) {
+        return PostPostLikeResDto.of(likeCount, liked);
+    }
+
 }
