039e83a89bbcfc9948ef4cc75559b36c1ef412c5
WonSeok-dd <cluejws@gmail.com>
2024-08-21 19:38:49 +0900

    [#6] test: 음원 게시물 관련(컨트롤러) 테스트 support 적용

---
+++ module-api/src/test/java/com/soundie/post/ControllerTestSupport.java
@@ -0,0 +1,24 @@
+package com.soundie.post;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.soundie.post.controller.PostController;
+import com.soundie.post.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = {
+        PostController.class
+})
+public abstract class ControllerTestSupport {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @MockBean
+    protected PostService postService;
+}
--- module-api/src/test/java/com/soundie/post/controller/PostControllerTest.java
+++ module-api/src/test/java/com/soundie/post/controller/PostControllerTest.java
@@ -1,17 +1,13 @@
 package com.soundie.post.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.soundie.member.domain.Member;
+import com.soundie.post.ControllerTestSupport;
 import com.soundie.post.domain.Post;
 import com.soundie.post.dto.*;
-import com.soundie.post.service.PostService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
@@ -26,16 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = PostController.class)
-class PostControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @MockBean
-    private PostService postService;
+class PostControllerTest extends ControllerTestSupport {
 
     @DisplayName("음원 게시물 목록을 조회한다.")
     @Test
