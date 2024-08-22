e4a4eff1f56091fda3897fedeedc0cef9c0aeb66
WonSeok-dd <cluejws@gmail.com>
2024-08-22 19:45:33 +0900

    [#6] test: 음원 게시물 관련 통합 테스트

--- module-api/src/test/java/com/soundie/post/ControllerTestSupport.java
+++ module-api/src/test/java/com/soundie/post/ControllerTestSupport.java
@@ -1,16 +1,13 @@
 package com.soundie.post;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.soundie.post.controller.PostController;
-import com.soundie.post.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(controllers = {
-        PostController.class
-})
+@SpringBootTest
+@AutoConfigureMockMvc
 public abstract class ControllerTestSupport {
 
     @Autowired
@@ -18,7 +15,4 @@ public abstract class ControllerTestSupport {
 
     @Autowired
     protected ObjectMapper objectMapper;
-
-    @MockBean
-    protected PostService postService;
 }
--- module-api/src/test/java/com/soundie/post/controller/PostControllerTest.java
+++ module-api/src/test/java/com/soundie/post/controller/PostControllerTest.java
@@ -1,36 +1,24 @@
 package com.soundie.post.controller;
 
-import com.soundie.member.domain.Member;
 import com.soundie.post.ControllerTestSupport;
-import com.soundie.post.domain.Post;
 import com.soundie.post.dto.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
-import java.util.List;
-
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = PostController.class)
 class PostControllerTest extends ControllerTestSupport {
 
     @DisplayName("음원 게시물 목록을 조회한다.")
     @Test
     void readPostList() throws Exception {
-        // given
-        GetPostResDto response = GetPostResDto.of(List.of());
-        given(postService.readPostList()).willReturn(response);
-
         // when // then
         mockMvc.perform(
                         get("/api/posts")
@@ -46,11 +34,7 @@ class PostControllerTest extends ControllerTestSupport {
     @Test
     void readPost() throws Exception {
         //given
-        Long memberId = 1L;
-        Long postId = 1L;
-
-        GetPostDetailResDto response = getPostDetailResDto(memberId, postId);
-        given(postService.readPost(any(), any())).willReturn(response);
+        Long postId = 1L; //pathVariable
 
         // when // then
         mockMvc.perform(
@@ -68,15 +52,12 @@ class PostControllerTest extends ControllerTestSupport {
     void createPost() throws Exception {
         // given
         Long memberId = 1L;
-        Long postId = 1L;
-
-        PostPostCreateReqDto request = postPostCreateReqDto();
-        PostIdElement response = postIdElement(postId);
-        given(postService.createPost(anyLong(), any(PostPostCreateReqDto.class))).willReturn(response);
-
-        // when // then
         MultiValueMap <String, String> params = new LinkedMultiValueMap<>();
         params.add("memberId", String.valueOf(memberId));
+
+        PostPostCreateReqDto request = postPostCreateReqDto();
+
+        // when // then
         mockMvc.perform(
                 post("/api/posts")
                         .params(params)
@@ -87,25 +68,20 @@ class PostControllerTest extends ControllerTestSupport {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.code").value("200"))
                 .andExpect(jsonPath("$.message").value("success"))
-                .andExpect(jsonPath("$.data.postId").value(1L));
-
+                .andExpect(jsonPath("$.data.postId").exists());
     }
 
     @DisplayName("음원 게시물에 좋아요를 누른다.")
     @Test
     void likePost() throws Exception {
         // given
+        Long postId = 1L; //pathVariable
+
         Long memberId = 1L;
-        Long postId = 1L;
-        Number likeCount = 1;
-        Boolean liked = true;
-
-        PostPostLikeResDto response = postPostLikeResDto(likeCount, liked);
-        given(postService.likePost(anyLong(), anyLong())).willReturn(response);
-
-        // when // then
         MultiValueMap <String, String> params = new LinkedMultiValueMap<>();
         params.add("memberId", String.valueOf(memberId));
+
+        // when // then
         mockMvc.perform(
                         post("/api/posts/" + postId + "/like")
                                 .params(params)
@@ -115,24 +91,8 @@ class PostControllerTest extends ControllerTestSupport {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.code").value("200"))
                 .andExpect(jsonPath("$.message").value("success"))
-                .andExpect(jsonPath("$.data.likeCount").value(1))
-                .andExpect(jsonPath("$.data.liked").value(true));
-    }
-
-    private GetPostDetailResDto getPostDetailResDto(Long memberId, Long postId) {
-        Member member = new Member("정원석");
-        member.setId(memberId);
-        Post post = new Post(
-                memberId,
-                "노래 제목",
-                member.getName(),
-                "노래 주소",
-                "앨범 이미지 주소",
-                "앨범 이름"
-        );
-        post.setId(postId);
-
-        return GetPostDetailResDto.of(post, member);
+                .andExpect(jsonPath("$.data.likeCount").exists())
+                .andExpect(jsonPath("$.data.liked").exists());
     }
 
     private PostPostCreateReqDto postPostCreateReqDto(){
@@ -143,13 +103,4 @@ class PostControllerTest extends ControllerTestSupport {
                 "앨범 이름"
         );
     }
-
-    private PostIdElement postIdElement(Long postId){
-        return PostIdElement.of(postId);
-    }
-
-    private PostPostLikeResDto postPostLikeResDto(Number likeCount, Boolean liked) {
-        return PostPostLikeResDto.of(likeCount, liked);
-    }
-
 }
