fb0e0873f5eda232a0347f2fd764550768a9fc35
WonSeok-dd <cluejws@gmail.com>
2024-08-29 01:24:09 +0900

    [#8] refactor: config 관련 파일 제거

--- module-api/src/main/java/com/soundie/ApiApplication.java
+++ module-api/src/main/java/com/soundie/ApiApplication.java
@@ -1,13 +1,9 @@
 package com.soundie;
 
-import com.soundie.global.config.Config;
-import com.soundie.global.config.MyBatisRepositoryConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
-@Import({Config.class, MyBatisRepositoryConfig.class})
-@SpringBootApplication(scanBasePackages = {"com.soundie.api"})
+@SpringBootApplication
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
