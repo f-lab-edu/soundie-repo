ada6e4ba4533620d38cd2415fb4cd2f8c2a721b8
WonSeok-dd <cluejws@gmail.com>
2024-08-28 19:53:10 +0900

    [#8] feat: 좋아요 관련(mybatis 레포지토리)

--- module-core/src/main/java/com/soundie/post/domain/PostLike.java
+++ module-core/src/main/java/com/soundie/post/domain/PostLike.java
@@ -2,15 +2,19 @@ package com.soundie.post.domain;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class PostLike {
 
     private Long id;
     private Long memberId;
     private Long postId;
+    private LocalDateTime createdAt;
 
     public PostLike(Long memberId, Long postId){
         this.memberId = memberId;
         this.postId = postId;
+        this.createdAt = LocalDateTime.now();
     }
 }
---
+++ module-core/src/main/java/com/soundie/post/mapper/PostLikeMapper.java
@@ -0,0 +1,23 @@
+package com.soundie.post.mapper;
+
+import com.soundie.post.domain.PostLike;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PostLikeMapper {
+
+    List<PostLike> findPostLikes();
+
+    Optional<PostLike> findPostLikeByMemberIdAndPostId(
+            @Param("member_id") Long memberId,
+            @Param("post_id") Long postId);
+
+    Long countPostLikesByPostId(@Param("post_id") Long postId);
+
+    void save(@Param("postlike") PostLike postLike);
+
+    void delete(@Param("postlike") PostLike postLike);
+
+}
