ebb381035d9e6312d59bb5ec4f75779e5509caad
WonSeok-dd <cluejws@gmail.com>
2024-09-02 04:10:32 +0900

    [#10] refactor: 알림 관련 dto innerBuilder 적용

--- module-core/src/main/java/com/soundie/notification/dto/GetNotificationResDto.java
+++ module-core/src/main/java/com/soundie/notification/dto/GetNotificationResDto.java
@@ -9,16 +9,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Getter
-@Builder
+@Builder(builderMethodName = "innerBuilder")
 public class GetNotificationResDto {
 
-    private Collection<GetNotificationElement> notifications;
+    private final Collection<GetNotificationElement> notifications;
+
+    private static GetNotificationResDtoBuilder builder(Collection<GetNotificationElement> notifications) {
+        return innerBuilder()
+                .notifications(notifications);
+    }
 
     public static GetNotificationResDto of(List<Notification> notifications) {
-        return GetNotificationResDto.builder()
-                .notifications(notifications.stream()
-                        .map(n -> GetNotificationElement.of(n))
-                        .collect(Collectors.toList()))
+        return GetNotificationResDto.builder(
+                    notifications.stream()
+                            .map(GetNotificationElement::of)
+                            .collect(Collectors.toList())
+                )
                 .build();
 
     }
