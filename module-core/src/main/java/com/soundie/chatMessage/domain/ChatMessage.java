2d14c44cb2c0bd76d163c9752a893c52c3ac6ea8
WonSeok-dd <cluejws@gmail.com>
2024-09-04 07:27:13 +0900

    [#14] fix: ChatMessage 필드 추가
    - Redis 에 저장을 위한 id 추가

--- module-core/src/main/java/com/soundie/chatMessage/domain/ChatMessage.java
+++ module-core/src/main/java/com/soundie/chatMessage/domain/ChatMessage.java
@@ -5,16 +5,18 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Getter
 @NoArgsConstructor
 public class ChatMessage implements Serializable {
 
+    private String id; // 메시지 Id(String=UUID)
     private ChatMessageType type; // 메시지 타입
-    private Long chatRoomId; // 채팅방 Id
-    private Long senderId; // 메시지 보낸 사람
-    private String content; // 메시지
-    private LocalDateTime createdAt; // 생성 시간
+    private Long chatRoomId; // 채팅방 Id(Long)
+    private Long senderId; // 메시지 보낸 사람 Id(Long)
+    private String content; // 메시지 내용
+    private LocalDateTime createdAt; // 메시지 생성 시간
 
     /*
     * 입장 메시지 위함
@@ -25,6 +27,7 @@ public class ChatMessage implements Serializable {
             Long chatRoomId,
             String content,
             LocalDateTime createdAt){
+        this.id = UUID.randomUUID().toString();
         this.type = type;
         this.chatRoomId = chatRoomId;
         this.content = content;
