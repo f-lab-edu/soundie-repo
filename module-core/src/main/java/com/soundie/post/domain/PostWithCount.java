bb0c3a851fe888a750d96937274765dedf10cdc7
WonSeok-dd <cluejws@gmail.com>
2024-09-13 06:46:11 +0900

    [#18] refactor: 불필요한 JsonSerialize, JsonDeserialize 제거 - 음원 게시물 목록 조회에 대한 Spring Cache 의   데이터 전체 Cache 가 아닌, Post Cache 로 인한 수정

--- module-core/src/main/java/com/soundie/post/domain/PostWithCount.java
+++ module-core/src/main/java/com/soundie/post/domain/PostWithCount.java
@@ -1,9 +1,5 @@
 package com.soundie.post.domain;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,9 +18,6 @@ public class PostWithCount {
     private String albumName;
     private Number likeCount;
     private Number commentCount;
-
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime createdAt;
 
     public PostWithCount(
