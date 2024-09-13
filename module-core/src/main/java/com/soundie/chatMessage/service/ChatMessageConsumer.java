fd0fee21cfd0e466086f8fba4f51e1442134ce9c
WonSeok-dd <cluejws@gmail.com>
2024-09-13 20:07:28 +0900

    [#20] fix: 불필요한 Config 및 Bean 제거
    - 실시간 통신은 WebSocket, Stomp → Polling 으로 수정

--- module-core/src/main/java/com/soundie/chatMessage/service/ChatMessageConsumer.java
+++ module-core/src/main/java/com/soundie/chatMessage/service/ChatMessageConsumer.java
@@ -4,15 +4,12 @@ import com.soundie.chatMessage.domain.ChatMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class ChatMessageConsumer {
-
-    private final SimpMessageSendingOperations messagingTemplate;
 
     /**
      * Kafka 에서 메시지가 발행(publish)되면 대기하고 있던 Kafka Consumer 가 해당 메시지를 받아 처리
@@ -21,9 +18,5 @@ public class ChatMessageConsumer {
     public void sendMessage(ChatMessage chatMessage) {
         log.info("messageType:{}, messageChatRoomId:{}", chatMessage.getType(),chatMessage.getChatRoomId());
         log.info("messageSenderId:{}, messageContent:{}", chatMessage.getSenderId(),chatMessage.getContent());
-        messagingTemplate.convertAndSend(
-                "/sub/chatRooms/" + chatMessage.getChatRoomId(),
-                chatMessage
-        );
     }
 }
