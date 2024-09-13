4ef255722a1d238da5e9c69c27c2d0924ec02843
WonSeok-dd <cluejws@gmail.com>
2024-09-13 23:50:21 +0900

    [#20] feat: 채팅 메시지 관련(MyBatis 레포지토리) 적용

---
+++ module-core/src/main/java/com/soundie/chatMessage/mapper/ChatMessageMapper.java
@@ -0,0 +1,17 @@
+package com.soundie.chatMessage.mapper;
+
+import com.soundie.chatMessage.domain.ChatMessage;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface ChatMessageMapper {
+
+    List<ChatMessage> findChatMessagesByChatRoomId(@Param("chatRoomId") Long chatRoomId);
+
+    void save(@Param("chatMessage") ChatMessage chatMessage);
+
+    void deleteChatMessagesByChatRoomId(@Param("chatRoomId") Long chatRoomId);
+}
---
+++ module-core/src/main/java/com/soundie/chatMessage/repository/MyBatisChatMessageRepository.java
@@ -0,0 +1,40 @@
+package com.soundie.chatMessage.repository;
+
+import com.soundie.chatMessage.domain.ChatMessage;
+import com.soundie.chatMessage.mapper.ChatMessageMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MyBatisChatMessageRepository implements ChatMessageRepository {
+
+    private final ChatMessageMapper chatMessageMapper;
+
+    /*
+     * 채팅방 Id로, 채팅 메시지 목록 조회
+     * */
+    @Override
+    public List<ChatMessage> findChatMessagesByChatRoomId(Long chatRoomId) {
+        return chatMessageMapper.findChatMessagesByChatRoomId(chatRoomId);
+    }
+
+    /*
+     * 채팅방 Id로, 채팅 메시지 저장
+     * */
+    @Override
+    public ChatMessage save(ChatMessage chatMessage) {
+        chatMessageMapper.save(chatMessage);
+        return chatMessage;
+    }
+
+    /*
+     * 채팅방 Id로, 채팅 메시지 목록 삭제
+     * */
+    @Override
+    public void deleteChatMessagesByChatRoomId(Long chatRoomId) {
+        chatMessageMapper.deleteChatMessagesByChatRoomId(chatRoomId);
+    }
+}
