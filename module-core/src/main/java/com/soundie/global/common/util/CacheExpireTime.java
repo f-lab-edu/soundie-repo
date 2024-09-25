7be1bd9d3cd84df45bdbe4e3fb35c86e382086b8
WonSeok-dd <cluejws@gmail.com>
2024-09-25 22:21:18 +0900

    [#36] refactor: 음원 게시물 좋아요 캐시 수정
    - 음원 게시물 좋아요 캐시
    - 캐시 적용하지 않음으로 수정

--- module-core/src/main/java/com/soundie/global/common/util/CacheExpireTime.java
+++ module-core/src/main/java/com/soundie/global/common/util/CacheExpireTime.java
@@ -6,7 +6,6 @@ public class CacheExpireTime {
     public static final Long POST_CURSOR = 1L;
     public static final Long COMMENT = 1L;
     public static final Long COMMENT_COUNT = 1L;
-    public static final Long LIKE = 1L;
     public static final Long LIKE_COUNT = 1L;
     public static final Long MEMBER = 24L;
 }
--- module-core/src/main/java/com/soundie/global/common/util/CacheNames.java
+++ module-core/src/main/java/com/soundie/global/common/util/CacheNames.java
@@ -6,7 +6,6 @@ public class CacheNames {
     public static final String POST_CURSOR = "post_cursor";
     public static final String COMMENT = "comment";
     public static final String COMMENT_COUNT = "comment_count";
-    public static final String LIKE = "like";
     public static final String LIKE_COUNT = "like_count";
     public static final String MEMBER = "member";
 }
