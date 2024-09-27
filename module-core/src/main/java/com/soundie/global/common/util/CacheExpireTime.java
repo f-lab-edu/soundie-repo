796e589ce91d7d58b8c37c655159120dec211b6c
WonSeok-dd <cluejws@gmail.com>
2024-09-28 01:21:36 +0900

    [#36] refactor: 음원 게시물의 댓글 목록(Cursor Pagination) 캐시 수정
    - 음원 게시물의 댓글 목록(Cursor Pagination) 캐시
    - Cursor 마다 캐시 아닌, 하나의 Custom 캐시로 수정

--- module-core/src/main/java/com/soundie/global/common/util/CacheExpireTime.java
+++ module-core/src/main/java/com/soundie/global/common/util/CacheExpireTime.java
@@ -4,7 +4,7 @@ public class CacheExpireTime {
 
     public static final Long POST = 5L;
     public static final Long POST_CURSOR = 1L;
-    public static final Long COMMENT = 5L;
+    public static final Long COMMENT_CURSOR = 1L;
     public static final Long COMMENT_COUNT = 1L;
     public static final Long LIKE_COUNT = 1L;
     public static final Long MEMBER = 5L;
--- module-core/src/main/java/com/soundie/global/common/util/CacheNames.java
+++ module-core/src/main/java/com/soundie/global/common/util/CacheNames.java
@@ -4,7 +4,7 @@ public class CacheNames {
 
     public static final String POST = "post";
     public static final String POST_CURSOR = "post_cursor";
-    public static final String COMMENT = "comment";
+    public static final String COMMENT_CURSOR = "comment_cursor";
     public static final String COMMENT_COUNT = "comment_count";
     public static final String LIKE_COUNT = "like_count";
     public static final String MEMBER = "member";
