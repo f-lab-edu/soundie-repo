39162a0537adc56d73e2c32d655d20bcd3982338
WonSeok-dd <cluejws@gmail.com>
2024-10-02 19:50:21 +0900

    [#40] feat: AWS S3 Config 설정

--- module-core/src/main/java/com/soundie/global/config/S3Config.java
+++ module-core/src/main/java/com/soundie/global/config/S3Config.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
@@ -22,6 +23,14 @@ public class S3Config {
     @Bean
     public S3Presigner s3Presigner() {
         return S3Presigner.builder()
+                .credentialsProvider(this::awsCredentials)
+                .region(Region.of(region))
+                .build();
+    }
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
                 .credentialsProvider(this::awsCredentials)
                 .region(Region.of(region))
                 .build();
