d75b3167760e6e17460c5299a95284d6576943c2
Won Seok Jeong <75558861+cluejws@users.noreply.github.com>
2024-10-30 23:06:30 +0900

    [#50] 배포 피드백 반영 (#51)
    * [#50] build: module-api 모듈로 Dockerfile 이동
    
    - module-api(build.gradle)
    - module-core(build.gradle)
     - module-api 모듈로 Dockerfile 이동에 따른 설정 변경
    
    - Dockerfile
     - module-api 모듈로 Dockerfile 이동에 따른 작성
    
    * [#50] build: docker-compose.yml 수정 및 포트 구체화
    
    * [#50] build: sql init

--- module-core/src/main/resources/sql/data-mysql.sql
+++ module-core/src/main/resources/sql/data-mysql.sql
@@ -1,4 +1,5 @@
-insert into member (name) values ('회원1');
+insert into member (name) values ('admin');
+insert into member (name) values ('회원2');
 
 insert into post (member_id, title, artist_name, music_path, album_img_path, album_name)
-values (1, '노래 제목1', '회원1', '~', '~~', '앨범 제목1');
+values (1, '노래 제목1', '회원2', '~', '~~', '앨범 제목1');
